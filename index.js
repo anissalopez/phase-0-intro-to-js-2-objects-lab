13098fa87e49cbeadd8c9b86fa42ad77c8e7b24c
anissalopez <anissa965@gmail.com>
2023-04-28 14:17:21 -0400

    objects lab

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-// Write your solution in this file!
-/*To start, assign an `employee` variable to an `Object` containing
-`name` and `streetAddress` keys; you can use whatever values you like. Use
-literal syntax to create your `Object`. Various updates will be applied to this
-variable (destructively and non-destructively) in this lab.*/
-let employee = {
+const employee = {
     name: 'Anissa',
-    streetAdress: 'Las Olas',
-    DateofBirth: '1993',
-    Wage: '100,000',
+    streetAddress: 'Fort Lauderdale'
 }
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    return {...employee, [key]: value,
-    };
+
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return {...employee, [key]:value
+    }
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key] = value;
+
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value
     return employee
 }
 
-function deleteFromEmployeeByKey(obj, key){ 
-    
-    let newEmployee = {...obj};
+const deleteFromEmployeeByKey = (employee, key) => {
+    let newEmployee = {...employee}
     delete newEmployee[key]
     return newEmployee
-};
-function  destructivelyDeleteFromEmployeeByKey(employee, key){
+}
 
+const destructivelyDeleteFromEmployeeByKey = (employee, key) =>
+{
     delete employee[key]
     return employee
 }
-
-
-
